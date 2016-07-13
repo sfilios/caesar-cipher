582097961c173a053954b4ccb192f78548e7c700
Stephen <sfilios@users.noreply.github.com>
2016-07-13 16:47:53 -0700

    Update cipher.py

--- cipher.py
+++ cipher.py
@@ -9,9 +9,9 @@ alphabet={'a':1, 'b':2, 'c':3, 'd':4, 'e':5, 'f':6, 'g':7, 'h':8, 'i':9, 'j':10,
 number_code={1:'a', 2:'b', 3:'c', 4:'d', 5:'e', 6:'f', 7:'g', 8:'h', 9:'i', 10:'j', 11:'k', 12:'l', 13:'m', 14:'n', 15:'o', 16:'p', 17:'q', 18:'r', 19:'s', 20:'t', 21:'u', 22:'v', 23:'w', 24:'x', 25:'y', 26:'z'}
 
 ##remove the spaces
-for letter in secret_message:
-	if letter==' ':
-		secret_message=
+##for letter in secret_message:
+##	if letter==' ':
+##		secret_message=
 
 def scrambler(secret_message):
     new_message=[]
